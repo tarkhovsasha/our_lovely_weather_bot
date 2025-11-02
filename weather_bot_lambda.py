32c81a7c9295d17fce6186e3c4ccee1c62fdc8c6
Alexander Tarkhov <191038348+tarkhovsasha@users.noreply.github.com>
2025-11-02 13:26:18 +0200

    Retrieve actual weather

--- weather_bot_lambda.py
+++ weather_bot_lambda.py
@@ -3,8 +3,13 @@ import json
 import os
 import requests
 
+OPENWEATHERMAP_ORG_APP_ID = os.environ.get('OPENWEATHERMAP_ORG_APP_ID')
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
+
+url_pref = 'https://api.openweathermap.org/data/2.5/weather?q='
+url_postf = '&units=metric&appid='
+
 
 def send_message(chat_id, text, reply_markup=None):
     """Send a message to Telegram chat"""
@@ -14,6 +19,7 @@ def send_message(chat_id, text, reply_markup=None):
         payload['reply_markup'] = reply_markup
     response = requests.post(url, json=payload)
     return response.json()
+
 
 def get_keyboard():
     """Create a custom keyboard with 4 city options"""
@@ -28,6 +34,58 @@ def get_keyboard():
         "one_time_keyboard": False
     }
     return keyboard
+
+
+def windrose(deg):
+    dirlist = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N']
+    num = int(deg + 22.5) // 45
+    return dirlist[num]
+
+
+def send_photo_from_url(chat_id, photo_url, caption=None):
+    """Send a photo from URL to Telegram chat"""
+    url = f"{TELEGRAM_API_URL}/sendPhoto"
+    payload = {
+        'chat_id': chat_id,
+        'photo': photo_url
+    }
+    if caption:
+        payload['caption'] = caption
+    
+    response = requests.post(url, json=payload)
+    return response.json()
+
+
+def check_weather(user_text):
+    url = url_pref + user_text.lower() + url_postf + OPENWEATHERMAP_ORG_APP_ID
+    res = 'Should be a city on earth'
+    
+    try:
+        response = requests.get(url)
+        name = response.json()["name"]
+        temp = str(int(round(response.json()["main"]["temp"] - 273.15)))
+        pressure = str(int(response.json()["main"]["pressure"] * 0.75006375541921))
+        humidity = str(response.json()["main"]["humidity"])
+        wind = str(int(round(response.json()["wind"]["speed"])))
+        wind_dir = windrose(response.json()["wind"]["deg"])
+        icon = response.json()["weather"][0]["icon"]
+        icon_url = 'https://openweathermap.org/img/wn/{}@4x.png'.format(icon)        
+        tz = response.json()["timezone"]
+        sunRise = datetime.fromtimestamp(response.json()["sys"]["sunrise"] + tz)
+        sunSet = datetime.fromtimestamp(response.json()["sys"]["sunset"] + tz)
+    except:
+        # bot.send_message(message.from_user.id, "Should be a city on earth", reply_markup=keyboard)
+        return res
+
+    res = '**' + name + '**\n'
+    res += 'Температура: {}°С\n'.format(temp)
+    res += 'Давление: {} мм\n'.format(pressure)
+    res += 'Влажность: {}%\n'.format(humidity)
+    res += 'Ветер: {} {} м/с\n'.format(wind_dir, wind)
+    res += 'Солнце всходит: {:%H:%M}\n'.format(sunRise)
+    res += 'Солнце заходит: {:%H:%M}\n'.format(sunSet)
+    return res
+
 
 def lambda_handler(event, context):
     """Main handler function for AWS Lambda"""
@@ -50,15 +108,11 @@ def lambda_handler(event, context):
         # Handle text messages
         if "text" in message:
             user_text = message["text"]
-            valid_cities = ['Istanbul', 'Moscow', 'Sofia', 'Thessaloniki']
+            # valid_cities = ['Istanbul', 'Moscow', 'Sofia', 'Thessaloniki']
             
-            if user_text in valid_cities:
-                response_text = f"You selected: {user_text}"
-                send_message(chat_id, response_text)
-            else:
-                error_text = "Should be a city on earth"
-                send_message(chat_id, error_text)
-        
+            response_text = check_weather(user_text)
+            send_message(chat_id, response_text)
+
         return {'statusCode': 200, 'body': json.dumps('Message processed successfully')}
         
     except Exception as e:
